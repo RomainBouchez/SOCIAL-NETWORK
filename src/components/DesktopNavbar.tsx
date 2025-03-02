08d6954c9343f495dd7d1b93f9cb2e3160c44e63
Romain BOUCHEZ <bouchez@et.esiea.fr>
2025-03-02 15:32:47 +0100

    add Prisma schema

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -7,6 +7,7 @@ import { currentUser } from "@clerk/nextjs/server";
 
 async function DesktopNavbar() {
   const user = await currentUser();
+  console.log("user is here", user);
 
   return (
     <div className="hidden md:flex items-center space-x-4">
